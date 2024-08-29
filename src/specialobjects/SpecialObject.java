808783f8813f75c02c602acf369b27fc1465159c
md_cess <141897197+mdcess@users.noreply.github.com>
2024-08-29 14:38:09 -0600

    Actualizar SpecialObject.java

--- src/specialobjects/SpecialObject.java
+++ src/specialobjects/SpecialObject.java
@@ -1,5 +1,25 @@
 package src.specialobjects;
 
-public class SpecialObject {
+public abstract class SpecialObject {
+    private String name;
+    private String pillForm
+    private Skill skill; 
 
+    public SpecialObject(String name, String pillForm, Skill skill){
+        this.name=name;
+        this.pillForm=pillForm;
+        this.skill=skill;
+    }
+
+    public String getNombre(){
+            return this.name;
+    }
+
+    public String getPillForm(){
+        return this.pillForm;
+    }
+
+    public Skill getSkill(){
+        return this.skill;
+    }
 }
