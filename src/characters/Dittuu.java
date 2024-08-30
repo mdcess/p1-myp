c9e1dcd576e8797a7b3aa4d15942032db886ea1c
Luis Solares <lm.solares@ciencias.unam.mx>
2024-08-30 12:51:11 -0600

    Dittuu

--- src/characters/Dittuu.java
+++ src/characters/Dittuu.java
@@ -3,6 +3,10 @@ package src.characters;
 /**
  * 
  */
-public class Dittuu extends Character {
+public class Dittuu extends Character implements ChingopokomonSkill {
+
+    public void copySkill(ChingopokomonSkill character){
+
+    }
 
 }
