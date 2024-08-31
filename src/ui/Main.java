5734d3b9131c90116149b615c878d11cb2b4efa0
Luis Solares <lm.solares@ciencias.unam.mx>
2024-08-31 02:10:46 -0600

    main

---
+++ src/ui/Main.java
@@ -0,0 +1,35 @@
+package src.ui;
+
+import src.battlefield.Battlefield;
+import src.battlefield.Viewer;
+import src.characters.Korby;
+import src.specialobjects.ElexirBottle;
+
+/**
+ * 
+ */
+public class Main {
+
+    private static Battlefield battle = new Battlefield();
+
+    public static void case01() {
+
+        Viewer obs1 = new Viewer("mdCess", "Korby");
+        Viewer obs2 = new Viewer("Luis", "MeganMan");
+        Viewer obs3 = new Viewer("Lupita", "Dittu");
+
+        battle.getFighters().get(0).attack(battle.getFighters().get(1));
+        battle.getFighters().set(1, battle.getFighters().get(0).attack(battle.getFighters().get(1)));
+        System.out.println("Korby ataco a MeganMan con " + battle.getFighters().get(0).getSkill().getAttackName());
+        System.out.println(battle.getFighters().get(1).toString());
+        battle.getFighters().get(0).setSkill(new ElexirBottle());
+        battle.getFighters().get(0).attack(battle.getFighters().get(1));
+        System.out.println("Korby ataco a MeganMan con " + battle.getFighters().get(0).getSkill().getAttackName());
+
+    }
+
+    public static void main(String[] args) {
+        case01();
+    }
+
+}
